684c3d63057e90474a3eec4017853338b45a8fb2
taher-dev <monmoyzx@gmail.com>
2025-11-04 18:22:41 +0600

    refactor: Clean up formatting and limit chat message width for better readability

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import userImage from '../assets/images/user.png';
-import chatbotImage from '../assets/images/chatbot.png'
+import React from "react";
+import userImage from "../assets/images/user.png";
+import chatbotImage from "../assets/images/chatbot.png";
 
 function ChatMessage({ message, sender }) {
   return (
     <>
-        {sender === 'chatbot' && (
-          <img 
-            src={chatbotImage} 
-            alt='chatbot' 
-            className='w-[50px] h-[50px] object-cover'
-          ></img>
-        )}
-        <div className='bg-gray-100 p-3 rounded-lg'>
-          {message}
-        </div>
-        {sender === 'user' && (
-          <img 
-            src={userImage} 
-            alt='user' 
-            className='w-[50px] h-[50px] object-cover'
-          ></img>
-        )}
+      {sender === "chatbot" && (
+        <img
+          src={chatbotImage}
+          alt="chatbot"
+          className="w-[50px] h-[50px] object-cover"
+        ></img>
+      )}
+      <div className="bg-gray-100 p-3 rounded-lg max-w-[70%]">{message}</div>
+      {sender === "user" && (
+        <img
+          src={userImage}
+          alt="user"
+          className="w-[50px] h-[50px] object-cover"
+        ></img>
+      )}
     </>
-  )
+  );
 }
 
 export default ChatMessage;
